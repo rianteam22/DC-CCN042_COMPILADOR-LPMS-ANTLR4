ce35bf42882410c40b06c1d903eb5627714c03c7
Maria <maria.ferreira@ufpi.edu.br>
2025-01-20 23:05:58 -0300

    Refactor TACGenerator

--- TACGenerator.py
+++ TACGenerator.py
@@ -34,16 +34,6 @@ class TACGenerator(GramaticaVisitor):
         for comando in ctx.comandos():
             self.visit(comando)
 
-    # def visitVarDecl(self, ctx: GramaticaParser.VarDeclContext):
-    #     for var in ctx.VARNAME():
-    #         self.emit(f"declare {var.getText()}")
-
-    # def visitConstDecl(self, ctx: GramaticaParser.ConstDeclContext):
-    #     for i in range(len(ctx.VARNAME())):
-    #         var = ctx.VARNAME(i).getText()
-    #         value = ctx.getChild(4 + i * 4).getText()
-    #         self.emit(f"const {var} = {value}")
-
     def visitOpMath(self, ctx: GramaticaParser.OpMathContext):
         var = ctx.VARNAME().getText()
         expr_result = self.visit(ctx.getChild(2))
@@ -52,32 +42,31 @@ class TACGenerator(GramaticaVisitor):
     def visitExpressaoAritmetica(self, ctx: GramaticaParser.ExpressaoAritmeticaContext):
         if len(ctx.children) == 1:
             return self.visit(ctx.getChild(0))
+        
         left = self.visit(ctx.getChild(0))
-        op = ctx.getChild(1).getText()
-        right = self.visit(ctx.getChild(2))
-        temp = self.new_temp()
-        self.emit(f"{temp} = {left} {op} {right}")
-        return temp
+        for i in range(1, len(ctx.children) - 1, 2):
+            op = ctx.getChild(i).getText()
+            right = self.visit(ctx.getChild(i + 1))
+            temp = self.new_temp()
+            self.emit(f"{temp} = {left} {op} {right}")
+            left = temp
+        
+        return left
 
     def visitTermo(self, ctx: GramaticaParser.TermoContext):
         if len(ctx.children) == 1:
             return ctx.getChild(0).getText()
+        
         left = self.visit(ctx.getChild(0))
-        op = ctx.getChild(1).getText()
-        right = self.visit(ctx.getChild(2))
-        temp = self.new_temp()
-        self.emit(f"{temp} = {left} {op} {right}")
-        return temp
+        for i in range(1, len(ctx.children) - 1, 2):
+            op = ctx.getChild(i).getText()
+            right = self.visit(ctx.getChild(i + 1))
+            temp = self.new_temp()
+            self.emit(f"{temp} = {left} {op} {right}")
+            left = temp
+        
+        return left
     
-    # def visitFator(self, ctx: GramaticaParser.FatorContext):
-    #     if len(ctx.children) == 1:
-    #         return ctx.getChild(0).getText()
-    #     left = self.visit(ctx.getChild(0))
-    #     op = ctx.getChild(1).getText()
-    #     right = self.visit(ctx.getChild(2))
-    #     temp = self.new_temp()
-    #     self.emit(f"{temp} = {left} {op} {right}")
-    #     return temp
     
     def visitFator(self, ctx: GramaticaParser.FatorContext):
         if len(ctx.children) == 1:
@@ -91,26 +80,11 @@ class TACGenerator(GramaticaVisitor):
         self.emit(f"{temp} = {left} {op} {right}")
         return temp
 
-    # def visitCondicional(self, ctx: GramaticaParser.CondicionalContext):
-    #     cond = self.visit(ctx.expressaoBooleana())
-    #     label_true = self.new_label()
-    #     label_end = self.new_label()
-        
-    #     self.emit(f"if {cond} goto {label_true}")
-    #     if ctx.getChild(6):
-    #         self.visit(ctx.getChild(6))
-    #         print(ctx.getChild(6))
-    #     self.emit(f"goto {label_end}")
-    #     self.emit(f"{label_true}:")
-    #     self.visit(ctx.comandos(0))
-    #     self.emit(f"{label_end}:")
 
     def visitCondicional(self, ctx: GramaticaParser.CondicionalContext):
         cond = self.visit(ctx.expressaoBooleana())
         label_else = self.new_label()
         label_end = self.new_label()
-        # for i in range(10):
-        #     print(ctx.getChild(i))
         
         self.emit(f"if not {cond} goto {label_else}")
         
@@ -137,8 +111,13 @@ class TACGenerator(GramaticaVisitor):
         self.emit(f"{label_end}:")
 
     def visitExpressaoBooleana(self, ctx: GramaticaParser.ExpressaoBooleanaContext):
+        #print("visitExpressaoBooleana")
         if len(ctx.children) == 1:
             return ctx.getChild(0).getText()
+        if len(ctx.children) == 2 and ctx.getChild(0).getText() == '!':
+            return f"not {self.visit(ctx.getChild(1))}"
+        if len(ctx.children) == 3 and ctx.getChild(0).getText() == '(':
+            return self.visit(ctx.getChild(1))
         left = self.visit(ctx.getChild(0))
         op = ctx.getChild(1).getText()
         right = self.visit(ctx.getChild(2))
@@ -146,14 +125,6 @@ class TACGenerator(GramaticaVisitor):
         self.emit(f"{temp} = {left} {op} {right}")
         return temp
 
-    # def visitFuncprint(self, ctx: GramaticaParser.FuncprintContext):
-    #     for expr in ctx.expressao():
-    #         result = self.visit(expr)
-    #         self.emit(f"print {result}")
-
-    # def visitFuncinput(self, ctx: GramaticaParser.FuncinputContext):
-    #     for var in ctx.VARNAME():
-    #         self.emit(f"input {var.getText()}")
 
 if __name__ == "__main__":
     from antlr4 import FileStream
